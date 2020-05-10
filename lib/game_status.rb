8700b6edb3c5eba8d5cdae20c9d6473b0abd518f
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-10 22:20:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,7 @@ def won?(board)
       return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
-    else
-      false
     end
   }
+  return false
 end
