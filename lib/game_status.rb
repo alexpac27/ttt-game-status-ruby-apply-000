6d2b165aa23f06dd6a1857e6216cd09d66d9675c
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-10 22:38:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,7 @@ end
 
 def full?(board)
 
-  if won?(board) == false && !board.include?("" || " " || nil)
+  if !won?(board) && !board.include?("" || " " || nil)
     return true
   else
     return false
